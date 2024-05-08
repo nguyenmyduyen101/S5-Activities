ecce7b07b16de31511b197d54582b26cc9babe9c
nguyenmyduyen101 <136870985+nguyenmyduyen101@users.noreply.github.com>
2024-05-08 14:36:11 +0700

    first commit

---
+++ exercice-1/main.js
@@ -0,0 +1,14 @@
+//  TODO 1 : In Math.js Export all  functions and constants
+
+//  TODO 2 : In main.js : Import all functions and constants from MATH module
+import { add, subtract, PI } from './math.js';
+
+console.log(add(5, 3)); // Output: 8
+console.log(subtract(10, 4)); // Output: 6
+console.log("PI = " + PI); // Output: 3.14159265359
+//  TODO 3 : In package.json add the following property         {  "type": "module"  }
+
+// TODO 4 : Test this code
+
+// Help  ?            https://www.scaler.com/topics/nodejs/require-vs-import-nodejs/
+
